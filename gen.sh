d09317d1621d8336190dd49cb2be4c42e264bd62
Yonggang Yu <yonggangyu@ucar.edu>
2022-05-23 17:44:58 -0600

    update gen.sh

--- gen.sh
+++ gen.sh
@@ -3,10 +3,10 @@ base=/glade/scratch/$USER/br_autotest/br_$mydate
 d_here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cat > crontab.txt <<EOF
-00 00 * * 1,2,3,4,5,6  $d_here/step1.sh
-15 00 * * 1,2,3,4,5,6  /opt/pbs/bin/qsub ${d_here}/job_make_ctest.scr
-30 01 * * 5,6           $d_here/step3.sh &> $d_here/out.3
-00 18 * * 5,6           $d_here/step4.sh &> $d_here/out.4
+30 12 * * 1,2,3,4,5,6  $d_here/step1.sh
+45 12 * * 1,2,3,4,5,6  /opt/pbs/bin/qsub ${d_here}/job_make_ctest.scr
+45 13 * * 5,6           $d_here/step3.sh &> $d_here/out.3
+00 20 * * 5,6           $d_here/step4.sh &> $d_here/out.4
 #
 #crontab $d_here/crontab.txt
 EOF
@@ -14,7 +14,7 @@ EOF
 cat > job_make_ctest.scr <<EOF
 #!/bin/bash
 #PBS -A NMMM0015
-#PBS -l walltime=00:29:00
+#PBS -l walltime=00:35:00
 #PBS -l select=1:ncpus=16:mpiprocs=16
 #PBS -N make_ctest
 #PBS -q premium
